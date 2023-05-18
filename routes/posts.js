84719d10c8dbcb9bcdc3f2687d09c1af5816890d
atsushi-12 <121540076+atsushi-12@users.noreply.github.com>
2023-05-18 09:04:55 +0900

    add PostMethod

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,28 @@
 //ユーザー情報の管理
 const router = require("express").Router();
+const post = require("../models/Post");
 
-router.get("/", (req, res) => {
-  res.send("user Router");
+router.post("/", async (req, res) => {
+  const newPost = new post(req.body);
+  try {
+    //newでインスタンスを作成した後は必ずsaveする
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  } catch (err) {
+    //500はサーバーエラー
+    return res.status(500).json(err);
+  }
 });
 
+
+//投稿を更新する
+router.put("/:id", async (req, res) => {
+  try {
+    //投稿のidを取得する
+    const post = await post.findById(req.params.id);
+    //投稿のユーザーidとログインしているユーザーidが一致するかどうかを確認する
+    if (post.userId === req.body.userId) {
+      //投稿を更新する
+      await post.updateOne({ $set: req.body });
+      
 module.exports = router;
