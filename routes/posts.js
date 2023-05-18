8d73be0f8c7189ac4217f918f6aff6afd3b43645
atsushi-12 <121540076+atsushi-12@users.noreply.github.com>
2023-05-18 16:45:55 +0900

    add watch timeline

--- routes/posts.js
+++ routes/posts.js
@@ -79,4 +79,24 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//タイムライン取得
+router.get("/timeline/all", async (req, res) => {
+  try {
+    //ログインしているユーザーの情報を取得する
+    const currentUser = await User.findById(req.body.userId);
+    //ログインしているユーザーの投稿を取得する
+    const userPosts = await Post.find({ userId: currentUser._id });
+    //ログインしているユーザーのフォローしているユーザーの投稿を取得する
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    //ログインしているユーザーの投稿とフォローしているユーザーの投稿を結合する
+    res.json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = router;
